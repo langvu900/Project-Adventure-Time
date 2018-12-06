78f16ed950c7af7ad79bf5860b26ef634bca0cca
phana <phana@192.168.0.69>
2018-12-06 17:25:40 +0100

    lol

---
+++ project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.umu_jep.atime.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.umu_jep.atime.AdTimeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AdTimeGame(), config);
+	}
+}
