3280131d54ac553bd0cce393c77fee281cb89a37
langvu900 <45390349+langvu900@users.noreply.github.com>
2018-12-20 23:03:08 +0100

    Add files via upload

---
+++ project-adventure-time-core/src/com/umu_jep/atime/AdTimeGame.java
@@ -0,0 +1,24 @@
+package com.umu_jep.atime;
+
+import com.badlogic.gdx.Game;
+/**import com.badlogic.gdx.Screen;*/
+
+import screens.SplashScreen;
+import screens.TitelScreen;
+
+public class AdTimeGame extends Game {
+	
+	public TitelScreen titel;
+	public SplashScreen splash;
+	/**private Screen test;*/
+
+	@Override
+	public void create() {
+		
+		titel = new TitelScreen(this);
+		
+	}
+	
+	
+
+}
