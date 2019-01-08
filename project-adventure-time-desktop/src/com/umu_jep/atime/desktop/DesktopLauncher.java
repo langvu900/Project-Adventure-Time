248582c44867502c1ed07de2b9000cfd6c6b8a26
Jukingbox <45312919+BLAMHeresy@users.noreply.github.com>
2019-01-08 13:58:15 +0100

    Specified window dimensions and title in Desktop Launcher

--- project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
+++ project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.umu_jep.atime.AdTimeGame;
 
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "Adventure Time!";
+		config.width = 800;
+		config.height = 600;
+		
 		new LwjglApplication(new AdTimeGame(), config);
 	}
 }
