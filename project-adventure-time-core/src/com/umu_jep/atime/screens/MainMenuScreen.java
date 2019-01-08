59a7580de75f20b5e1a963ba3874764200eefe3e
Jukingbox <45312919+BLAMHeresy@users.noreply.github.com>
2019-01-08 13:11:57 +0100

    Slight edits for MainMenuScreen

---
+++ project-adventure-time-core/src/com/umu_jep/atime/screens/MainMenuScreen.java
@@ -0,0 +1,54 @@
+package com.umu_jep.atime.screens;
+
+import com.badlogic.gdx.Screen;
+
+public class MainMenuScreen implements Screen{
+
+	public MainMenuScreen () {
+		
+		//Constructor for MainMenuScreen
+	}
+	
+	@Override
+	public void dispose() {
+		
+		
+	}
+
+	@Override
+	public void hide() {
+		
+		
+	}
+
+	@Override
+	public void pause() {
+		
+		
+	}
+
+	@Override
+	public void render(float arg0) {
+		
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		
+		
+	}
+
+	@Override
+	public void resume() {
+		
+		
+	}
+
+	@Override
+	public void show() {
+		
+		
+	}
+
+}
