d58e97e5dec3d1e5ef05e9aec5dbdce1bc163e97
Jukingbox <45312919+BLAMHeresy@users.noreply.github.com>
2019-01-10 16:03:22 +0100

    More NullPointerExceptions.

--- project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
+++ project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.umu_jep.atime.desktop;
 
+//import java.awt.Dimension;
+//import java.awt.Toolkit;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -10,9 +13,13 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		config.title = "Adventure Time!";
-		Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //causes NullPointerException
+		//Gdx.graphics.setWindowedMode((int) screenSize.getWidth(), (int) screenSize.getHeight()); //causes NullPointerException as well
+		//probably caused by setWindowedMode()
 		
 		new LwjglApplication(new AdTimeGame(), config);
 	}
