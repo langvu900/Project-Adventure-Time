eb9e4f61e1dfd13d1ab6b41233bc45eedfef4e67
Jukingbox <45312919+BLAMHeresy@users.noreply.github.com>
2019-01-15 13:34:24 +0100

    Game window now scales to screen size.

--- project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
+++ project-adventure-time-desktop/src/com/umu_jep/atime/desktop/DesktopLauncher.java
@@ -1,9 +1,8 @@
 package com.umu_jep.atime.desktop;
 
-//import java.awt.Dimension;
-//import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.umu_jep.atime.AdTimeGame;
@@ -13,13 +12,14 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		
 		config.title = "Adventure Time!";
 		
-		Gdx.graphics.setWindowedMode(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //causes NullPointerException
-		//Gdx.graphics.setWindowedMode((int) screenSize.getWidth(), (int) screenSize.getHeight()); //causes NullPointerException as well
-		//probably caused by setWindowedMode()
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension dim = tk.getScreenSize();
+		
+		config.width = (int) dim.getWidth();
+		config.height = (int) dim.getHeight();
 		
 		new LwjglApplication(new AdTimeGame(), config);
 	}
