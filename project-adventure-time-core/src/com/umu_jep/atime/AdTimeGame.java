f24edc2e5a261b11d9c20486971aafbe0a9bc2d2
Jukingbox <45312919+BLAMHeresy@users.noreply.github.com>
2019-01-15 14:11:23 +0100

    shortcut

--- project-adventure-time-core/src/com/umu_jep/atime/AdTimeGame.java
+++ project-adventure-time-core/src/com/umu_jep/atime/AdTimeGame.java
@@ -1,43 +1,4 @@
-main-menu-screen
-package com.umu_jep.atime;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public class AdTimeGame extends ApplicationAdapter {
-	
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		
-		batch = new SpriteBatch();
-		img = new Texture("AT_Main_Menu.png");
-	}
-
-	@Override
-	public void render () {
-		
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		
-		batch.dispose();
-		img.dispose();
-	}
-}
-
-/*
 package com.umu_jep.atime;
 
 import com.badlogic.gdx.Game;
@@ -59,4 +20,3 @@ public class AdTimeGame extends Game {
 		
 	}
 }
-*/
